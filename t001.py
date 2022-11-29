ade76956bdc5c9c213a91f9757208603c3fe9918
Abashin Aleksey <mmilasomm@gmail.com>
2022-11-29 16:31:16 +0300

    изменили 1 задачу

--- t001.py
+++ t001.py
@@ -3,14 +3,14 @@
 # - 6 -> да
 # - 7 -> да
 # - 1 -> нет
-day = int(input('Ведите число дня недели:'))
-if day > 0 and day <= 7:
-    if day > 0 and day <= 5:
-        print('рабочие дни')
-    else:
-        print('выходной')
-else:
-    print('нет такого дня недели')
+# day = int(input('Ведите число дня недели:'))
+# if day > 0 and day <= 7:
+#     if day > 0 and day <= 5:
+#         print('рабочие дни')
+#     else:
+#         print('выходной')
+# else:
+#     print('нет такого дня недели')
 
 
 # day = int(input("Введите день недели  : "))
@@ -22,21 +22,21 @@ else:
 # else:
 #     print("введите другой день недели")
 
-# day = int(input("Введите день недели  : "))
-# match day:
-#     case 1:
-#         print('понедельник')
-#     case 2:
-#         print('вторник')
-#     case 3:
-#         print('среда')
-#     case 4:
-#         print('четверг')
-#     case 5:
-#         print('пятница')
-#     case 6:
-#         print('суббота')
-#     case 7:
-#         print('воскресенье')
-#     case _:
-#         print('не такого дня')
+day = int(input("Введите день недели  : "))
+match day:
+    case 1:
+        print('понедельник')
+    case 2:
+        print('вторник')
+    case 3:
+        print('среда')
+    case 4:
+        print('четверг')
+    case 5:
+        print('пятница')
+    case 6:
+        print('суббота')
+    case 7:
+        print('воскресенье')
+    case _:
+        print('не такого дня')
