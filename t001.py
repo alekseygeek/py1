977cb53d37491022310db228d08e3a0cba3ffa3f
Abashin Aleksey <mmilasomm@gmail.com>
2022-11-29 11:02:55 +0300

    решили 1 задачу

--- t001.py
+++ t001.py
@@ -11,3 +11,32 @@ if day > 0 and day <= 7:
         print('выходной')
 else:
     print('нет такого дня недели')
+
+
+# day = int(input("Введите день недели  : "))
+# if 0 < day < 8:
+#     if day == 6 or day == 7:
+#         print('выходной')
+#     else:
+#         print('будни')
+# else:
+#     print("введите другой день недели")
+
+# day = int(input("Введите день недели  : "))
+# match day:
+#     case 1:
+#         print('понедельник')
+#     case 2:
+#         print('вторник')
+#     case 3:
+#         print('среда')
+#     case 4:
+#         print('четверг')
+#     case 5:
+#         print('пятница')
+#     case 6:
+#         print('суббота')
+#     case 7:
+#         print('воскресенье')
+#     case _:
+#         print('не такого дня')
