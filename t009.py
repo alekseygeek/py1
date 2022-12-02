99f6058d720c704b397ffeb0c7020b74b327f681
Abashin Aleksey <mmilasomm@gmail.com>
2022-12-02 14:43:43 +0300

    дорешали 9 задачу

--- t009.py
+++ t009.py
@@ -2,9 +2,15 @@
 # Пример:
 # - [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
 
-num = [2, 3, 5, 9, 3]
-summ = 0
-for i in range(1, len(num), 2):
-    summ += num[i]
-print(f'{num}  сумма элементов списка, стоящих на нечётной позиции: {summ}')  
-  
+# num = [2, 3, 5, 9, 3]
+# summ = 0
+# for i in range(1, len(num), 2):
+#     summ += num[i]
+# print(f'{num}  сумма элементов списка, стоящих на нечётной позиции: {summ}')
+
+a = input(' введите число через пробел: ').split()
+i = 1
+while (i < len(a)//2+len(a) % 2):
+    num = len(a) - i-1
+    print(int(a[i])*int(a[num]), end=" ")
+    i+=1
