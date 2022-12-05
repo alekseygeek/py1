417345da705dc910d575170575ed044258973e4e
Abashin Aleksey <mmilasomm@gmail.com>
2022-12-05 10:40:56 +0300

    решили 16 задачу

--- t016.py
+++ t016.py
@@ -2,3 +2,39 @@
 # (Вывод тех элементов, которые были без повторов)
 # Ввод: 1 2 3 2 4 4
 # Вывод: 1 3
+
+
+order = [1, 2, 3, 2, 4, 4]
+
+answer = []
+deleted = []
+
+for item in order:
+    if item in deleted:
+        continue
+    if item in answer:
+        deleted.append(answer.pop(answer.index(item)))
+    else:
+        answer.append(item)
+
+print(answer)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# my_list = list(map(int, input('введите числа через пробел: \n').split()))
+# print(f'исходный список:{my_list}')
+# new_list = []
+# [new_list.append(i)for i in my_list if i not in new_list]
+# print(f"Список из неповторяющихся элементов: {new_list}")
