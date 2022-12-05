c6dcee9a6a1a2e21fb04395efdff53f3c13e14fa
Abashin Aleksey <mmilasomm@gmail.com>
2022-12-05 11:29:48 +0300

    два примера 5 задача

--- t005.py
+++ t005.py
@@ -2,10 +2,20 @@
 # Пример:
 # пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 
-number = int(input('введите число N :'))
-answer = []
-number1 = 1
-for i in range(1, number+1):
-    number1 *= i
-    answer.append(number1)
-print(answer)
+# number = int(input('введите число N :'))
+# answer = []
+# number1 = 1
+# for i in range(1, number+1):
+#     number1 *= i
+#     answer.append(number1)
+# print(answer)
+
+# второй вариант
+
+num = int(input())
+m = 1
+mass = []
+for i in range(1, num+1):
+    m = m*i
+    mass.append(m)
+print(mass)
